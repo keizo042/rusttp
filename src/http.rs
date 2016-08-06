bbb4b2977e84811384850636f23ad1b6e76fe1f3
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-08-06 23:49:39 +0900

    GET All body

--- src/http.rs
+++ src/http.rs
@@ -1,23 +1,29 @@
 use std::net::TcpStream;
 use std::result::Result;
 use std::io::{Write,Read};
+use std::io;
 use std::string::String;
+use std::vec::Vec;
 
-pub fn send(dist: String, port: u64) -> String
-{
-    let mut stream = TcpStream::connect(format!("{}:{}",dist, port).as_str()).unwrap();
-
-    let _ = stream.write(format!("{}{}{}{}", "GET / HTTP/1.1\nHost", dist.as_str(), "Connection:close\n" , "\n").as_bytes());
-    let mut s = String::with_capacity(4096);
-    let mut t = stream.take(4096);
-    let _ = t.read_to_string(&mut s);
-    return s;
+struct Conn{
+    body : String,
 }
 
-pub fn connect(dist :String, port: u64)
-{
+pub fn send(dist: String, port: u64) -> Option<String>{
+    let mut stream = match TcpStream::connect(format!("{}:{}",dist, port).as_str()) {
+        Ok(v) =>  v,
+        Err(e) => return None,
+    };
+
+    let t = format!("{}{}{}{}", "GET / HTTP/1.1\nHost", dist.as_str(), "Connection:close\n" , "\n");
+    let _ = stream.write(t.as_bytes());
+    let mut v: Vec<u8> = Vec::new();
+    let _ = stream.read_to_end(&mut v);
+     match String::from_utf8(v) {
+        Ok(v) => return Some(v),
+        Err(e) => return None,
+    };
 }
-    
 
 #[cfg(test)]
 mod tests {
