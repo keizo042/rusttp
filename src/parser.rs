025e9821a453b4b186eede3fd0f0193e9574264a
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-08-18 23:26:55 +0900

    [WIP]

--- src/parser.rs
+++ src/parser.rs
@@ -4,21 +4,22 @@ use std::hash;
 
 
 struct Parser {
-        body :& [u8],
-        state : usize,
+        body : &[u8],
+        state : fn() -> usize,
         pos : usize,
         len : usize,
 }
 
-trait Parser {
+trait Parseable {
+    fn new() -> self
+    fn start(&mut self) -> ()
+    fn emit() -> ()
+    fn identify() -> ()
+    fn header() -> ()
+    fn hname() -> ()
+    fn hvalue() -> ()
+    fn body() -> ()
+}
 
-    pub fn start() {
-    }
-
-    fn header(mut& self) {
-    }
-
-    fn body(mut& self) {
-    }
-
+impl Parser for Parseable {
 }
